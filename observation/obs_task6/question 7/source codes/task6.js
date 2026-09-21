d11eb1710e2c530cc3c7631cc5c95cded4e6fbd7
somyadaggubilli-art <somyadaggubilli29@gmail.com>
2026-09-21 23:56:55 +0530

    updated

--- observation/obs_task6/question 7/source codes/task6.js
+++
@@ -1,5 +0,0 @@
-const chalk = require("chalk");
-
-console.log(chalk.green("NPM package installed successfully!"));
-console.log(chalk.blue("This message is displayed using Chalk."));
-console.log(chalk.yellow("Node.js can use external packages."));
